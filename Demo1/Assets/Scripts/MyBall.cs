c46cc529f457b3863eb7ff98952e2ea9c66f1390
scym12 <scym@naver.com>
2020-12-27 13:05:50 +0900

    [유니티 기초 - B10] 힘을 이용하여 물체 움직여보기 (AddForce, AddTorque)

---
+++ Demo1/Assets/Scripts/MyBall.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBall : MonoBehaviour
+{
+    Rigidbody rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        // rig.velocity = new Vector3(3,3,3); // Vector3.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Update3();
+    }
+
+    void Update1() {        
+        // 1. 속력바꾸기
+        rig.velocity = Vector3.right;
+    }
+
+    void Update2() {
+        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        rig.AddForce(vec,ForceMode.Impulse);
+
+        // 2. 힘을 가하기, (Space, 상하좌우)
+        if(Input.GetButtonDown("Jump")) {
+            // 해당 방향으로 힘을 준다. 
+            rig.AddForce(Vector3.up * 50,ForceMode.Impulse);
+        }        
+    }
+
+    void Update3() {
+        // 3. 회전력
+        rig.AddTorque(Vector3.up);
+    }
+
+
+    void FixedUpdate() {  
+        // rig.velocity = new Vector3(3,3,3); // Vector3.right;
+    }
+}

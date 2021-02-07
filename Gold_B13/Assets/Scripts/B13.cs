d7dd967ba3f339ddd2351cceece722d15bf7c741
scym12 <scym@naver.com>
2021-02-07 18:06:32 +0900

    Unity : 골드메탈 : B13

---
+++ Gold_B13/Assets/Scripts/B13.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B13 : MonoBehaviour
+{
+    // Image 파일 읽어올때 설정법
+        // Texture Type : Sprite(2D and UI)
+        // Pixel Per Unit : 16  (해당 이미지가 16X16이미지일경우)
+        // Filter Mode : Point(no filter)
+        // Compression : None 
+
+
+
+    // Project Setting / Physics 2D // Default Contace Offset : 0.0001 
+}

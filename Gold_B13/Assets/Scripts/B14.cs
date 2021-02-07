ea115f2d435351d5ca178ce5b1d418c493429846
devgame2020 <scym12@gmail.com>
2021-02-07 19:58:38 +0900

    Unity : 골드메탈 : B14

---
+++ Gold_B13/Assets/Scripts/B14.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B14 : MonoBehaviour
+{
+    // Image 묶음을 읽을때
+
+    // Sprite Editor 
+        // Slice
+            // Grid By Sell Size 
+                // Pixel Size : 16 , 16
+                // Padding : 1,1
+
+
+
+    // Stats 
+        // Batches : DrawCall (값이 낮을수록 좋음)
+            // => 그래픽을 그리기 위해 메모리와 Cpu를 사용하는 횟수 
+
+
+
+
+    // Animation Controller
+    // Animation Clip
+}

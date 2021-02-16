8b5d48785a127f9f44fe1a09588b3922067195a6
devgame2020 <scym12@gmail.com>
2021-02-16 22:15:02 +0900

    Unity : 골드메탈 : B20

---
+++ Gold_B20/Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    float h;
+    float v;
+    Rigidbody2D rigid;
+
+    float speed;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        speed = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal") * speed;
+        v = Input.GetAxisRaw("Vertical") * speed;
+    }
+
+    void FixedUpdate() {
+        rigid.velocity = new Vector2(h,v);
+    }
+}

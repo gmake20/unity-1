7ba3d78d71dc4dc2f9e9236fdc407e0e08b64329
devgame2020 <scym12@gmail.com>
2021-02-18 22:19:40 +0900

    Unity : 골드메탈 : B23 : 캐릭터 대화창

--- Gold_B20/Assets/Scripts/GameManager.cs
+++ Gold_B20/Assets/Scripts/GameManager.cs
@@ -5,10 +5,14 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public TalkManager talkManager;
+    public int talkIndex; 
     public GameObject talkPanel;
     public Text talkText;
     public GameObject scanObject;
     public bool isAction;
+
+    public Image talkImg;
 
     public string hello() {
         string[] arr = new string[] { "좋은날씨네요." , "안녕하세요." , "오늘도 즐거운 하루~" ,"잘지내고 있죠?" };
@@ -16,22 +20,40 @@ public class GameManager : MonoBehaviour
     }
 
     public void Action(GameObject scanObj) {
-        if(isAction) {
-            isAction = false; 
-        }
-        else {
-            isAction = true;
-            scanObject = scanObj;
+        scanObject = scanObj;
 
-            
-            talkText.text = "[" + scanObj.name + "] " + hello();
-        }
+        ObjData objData = scanObject.GetComponent<ObjData>();
+        Talk(objData.id, objData.isNpc);
+        // talkText.text = "[" + scanObj.name + "] " + hello();
+
         talkPanel.SetActive(isAction);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Talk(int id, bool isNpc) {
+        string talkData = talkManager.GetTalk(id,talkIndex);
+        //Debug.Log(talkData);
+        //Debug.Log("index:" + talkIndex);
+
+        if(talkData == null) {
+            isAction = false;
+            talkIndex = 0;
+            return;
+        }
+
+        if(isNpc) {
+            var arr = talkData.Split(':');
+            talkText.text = arr[0];
+            talkImg.sprite = talkManager.GetImage(id,int.Parse(arr[1]));
+            talkImg.color = new Color(1,1,1,1);
+        }
+        else {
+            talkText.text = talkData;
+            talkImg.color = new Color(1,1,1,0);
+        }
+
+        isAction = true;
+        talkIndex++;
     }
+
+
 }
---
+++ Gold_B20/Assets/Scripts/TalkManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkManager : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData;
+    Dictionary<int,Sprite> talkImgData;
+
+    public Sprite[] imgArr;
+
+    void Awake() {
+        talkData = new Dictionary<int, string[]>();
+        talkImgData = new Dictionary<int, Sprite>();
+        GenerateData();
+    }
+
+    public void GenerateData() {
+        talkData.Add(1000, new string[] { "안녕?:0" , "이 곳에 처음 왔구나?:1"});
+        talkData.Add(2000, new string[] { "여어~:1" ,"이 호수는 정말 아름답지?:0", "사실 이 호수에는 무언가의 비밀이 숨겨져 있다고해.:1"});
+
+        talkData.Add(100, new string[] { "평범한 나무상자다" });
+        talkData.Add(200, new string[] { "누군가가 사용했던 흔적이 있는 책상이다." });
+
+        talkImgData.Add(1000 + 0,imgArr[0]);
+        talkImgData.Add(1000 + 1,imgArr[1]);
+        talkImgData.Add(1000 + 2,imgArr[2]);
+        talkImgData.Add(1000 + 3,imgArr[3]);
+        talkImgData.Add(2000 + 0,imgArr[4]);
+        talkImgData.Add(2000 + 1,imgArr[5]);
+        talkImgData.Add(2000 + 2,imgArr[6]);
+        talkImgData.Add(2000 + 3,imgArr[7]);
+    }
+
+    public string GetTalk(int id, int talkIndex) {
+        // Debug.Log(id+":" + talkIndex);
+        if(talkIndex == talkData[id].Length)
+            return null;
+        else
+             return talkData[id][talkIndex];
+    }
+
+    public Sprite GetImage(int id,int imgIndex) {
+        return talkImgData[id+imgIndex];
+    }
+
+}

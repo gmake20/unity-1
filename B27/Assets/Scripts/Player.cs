6edb50c8fe290bbad73a474db9f16ce84da46670
devgame2020 <scym12@gmail.com>
2021-03-04 22:30:28 +0900

    Unity : 골드메탈 : B27

---
+++ B27/Assets/Scripts/Player.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchRight;
+    public bool isTouchLeft;
+
+    Animator anim;
+    
+    void Awake() {
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        if( (isTouchRight && h == 1) || (isTouchLeft && h == -1))  h = 0;
+
+        float v = Input.GetAxisRaw("Vertical");
+        if( (isTouchTop && v == 1) || (isTouchBottom && v == -1))  v = 0;
+
+        Vector3 curPos = transform.position;
+        Vector3 nextgPos = new Vector3(h,v,0) * speed * Time.deltaTime;
+
+        transform.position = curPos + nextgPos;
+        
+        if(Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal")) {
+            anim.SetInteger("Input", (int)h);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        if(coll.gameObject.tag == "Border") {
+            switch(coll.gameObject.name) {
+                case "Top":
+                    isTouchTop = true;
+                    break;
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+                case "Right":
+                    isTouchRight = true;
+                    break;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll) {
+        if(coll.gameObject.tag == "Border") {
+            switch(coll.gameObject.name) {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+            }
+        }
+    }    
+}

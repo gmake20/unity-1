c82bc419a2117c082e22456993d3356ef5f3351d
andy <scym14@gmail.com>
2025-01-05 14:42:20 +0900

    김치런 게임 1차 커밋, 리소스 불러와서 배경 스크롤하기

---
+++ Kimchi-Run/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("Scroll 속도가 얼마나 빨라야하는가?")]
+    public float scrollSpeed;
+
+    [Header("References")]
+    public MeshRenderer meshRenderer;
+
+    void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(scrollSpeed * Time.deltaTime , 0);
+    }
+}
+
